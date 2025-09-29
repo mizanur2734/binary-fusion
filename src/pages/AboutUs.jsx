750ede337c5dc1644261b9479534f00c2e535687
mizanur2734 <mizanurbb2791@gmail.com>
2025-09-30 02:25:42 +0600

    Add Dream Big section with 4 responsive cards

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,6 +4,10 @@ import man2 from "../assets/about/who_are_we-2.png";
 import man3 from "../assets/about/who_are_we-3.png";
 import man4 from "../assets/about/bulbul-ahmed.jpg";
 import man5 from "../assets/about/bulbul-ahmed.png";
+import man6 from "../assets/about/youth.png";
+import man7 from "../assets/about/adaptation.png";
+import man8 from "../assets/about/help.png";
+import man9 from "../assets/about/chatting.png";
 
 const AboutUs = () => {
   return (
@@ -39,7 +43,7 @@ const AboutUs = () => {
       </section>
       {/* 2 */}
       <section>
-        <div className="flex flex-col-reverse md:flex-row justify-between lg:gap-40 gap-10 lg:mt-26 mt-8">
+        <div className="flex flex-col-reverse md:flex-row justify-between lg:gap-40 gap-10 lg:mt-24 mt-8">
           {/* Image */}
           <div className="lg:max-w-[500px] mt-6 md:mt-0">
             <img src={man2} alt="" className="w-full h-auto" />
@@ -65,7 +69,7 @@ const AboutUs = () => {
       </section>
       {/* 3 */}
       <section>
-        <div className="flex flex-col md:flex-row items-center justify-between lg:gap-40 gap-10 lg:mt-26 mt-8">
+        <div className="flex flex-col md:flex-row items-center justify-between lg:gap-40 gap-10 lg:mt-20 mt-8">
           {/* Text */}
           <div className="lg:max-w-[600px]">
             <h2 className="lg:text-4xl text-3xl font-bold lg:leading-12">
@@ -90,8 +94,8 @@ const AboutUs = () => {
       </section>
       {/* 4 */}
       <section>
-        <div className="flex flex-col md:flex-row items-center justify-between lg:gap-40 gap-10 lg:mt-26 mt-8">
-            {/* Image */}
+        <div className="flex flex-col md:flex-row items-center justify-between lg:gap-40 gap-10 lg:mt-30 mt-8">
+          {/* Image */}
           <div className="lg:max-w-[500px] mt-6 md:mt-0">
             <img src={man4} alt="" className="w-full h-auto" />
           </div>
@@ -112,9 +116,74 @@ const AboutUs = () => {
               So ditch the compromises and join us! Let's build something truly
               amazing together!
             </p>
-               <img className="mt-6" src={man5} alt="" />
-               <h4 className="mt-4 text-xl font-bold">Bulbul Ahmed</h4>
-               <h5 className="text-lg font-bold opacity-70">Founder & CEO, Binary Fusion</h5>
+            <img className="mt-6" src={man5} alt="" />
+            <h4 className="mt-4 text-xl font-bold">Bulbul Ahmed</h4>
+            <h5 className="text-lg font-bold opacity-70">
+              Founder & CEO, Binary Fusion
+            </h5>
+          </div>
+        </div>
+      </section>
+      {/* dream big */}
+      <section className="lg:mt-26 mt-8">
+        <div className="max-w-6xl mx-auto px-4">
+          <h2 className="lg:text-4xl text-3xl font-bold text-center mb-10">
+            We <span className="text-green-600">Dream Big</span> But Act with{" "}
+            <span className="text-green-600">Honesty</span>
+          </h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 justify-items-center mt-20 lg:space-y-17 space-y-12">
+            {/* Card 1 */}
+            <div className="flex flex-col items-center text-center max-w-lg">
+              <img src={man6} className="h-20 mb-4" alt="man1" />
+              <h4 className="text-[22px] font-semibold mb-2 opacity-80">
+                Seeing the Human Side of Business
+              </h4>
+              <p className="text-lg opacity-70">
+                Teamwork makes the dream work! We're all different, but that's
+                what makes us strong. We use our unique skills to help each
+                other out and reach our goals. That’s our biggest strength!
+              </p>
+            </div>
+
+            {/* Card 2 */}
+            <div className="flex flex-col items-center text-center max-w-lg">
+              <img src={man7} className="h-20 mb-4" alt="man2" />
+              <h4 className="text-[22px] font-semibold mb-2 opacity-80">
+                Seeing the Human Side of Business
+              </h4>
+              <p className="text-lg opacity-70">
+                Teamwork makes the dream work! We're all different, but that's
+                what makes us strong. We use our unique skills to help each
+                other out and reach our goals. That’s our biggest strength!
+              </p>
+            </div>
+
+            {/* Card 3 */}
+            <div className="flex flex-col items-center text-center max-w-lg">
+              <img src={man8} className="h-20 mb-4" alt="man3" />
+              <h4 className="text-[22px] font-semibold mb-2 opacity-80">
+                Seeing the Human Side of Business
+              </h4>
+              <p className="text-lg opacity-70">
+                Teamwork makes the dream work! We're all different, but that's
+                what makes us strong. We use our unique skills to help each
+                other out and reach our goals. That’s our biggest strength!
+              </p>
+            </div>
+
+            {/* Card 4 */}
+            <div className="flex flex-col items-center text-center max-w-lg">
+              <img src={man9} className="h-20 mb-4" alt="man4" />
+              <h4 className="text-[22px] font-semibold mb-2 opacity-80">
+                Seeing the Human Side of Business
+              </h4>
+              <p className="text-lg opacity-70">
+                Teamwork makes the dream work! We're all different, but that's
+                what makes us strong. We use our unique skills to help each
+                other out and reach our goals. That’s our biggest strength!
+              </p>
+            </div>
           </div>
         </div>
       </section>
