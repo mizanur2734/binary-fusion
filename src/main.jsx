9a971c6845ac92281ec18702dd77162f8fe6b1c6
mizanur2734 <mizanurbb2791@gmail.com>
2025-10-08 17:32:22 +0600

    update main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router'
-import { router } from './router/router.jsx'
+import React, { StrictMode, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { router } from "./router/router.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+// Optional: Initialize AOS globally once
+function Root() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: true,
+    });
+  }, []);
+
+  return <RouterProvider router={router} />;
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />,
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);
